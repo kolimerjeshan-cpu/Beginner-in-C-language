415fe22ec6cae2e988fc7a6b11ff3ee4b3d9ff92
ahyoungyoung <kolimerjeshan@gmail.com>
2026-02-10 23:53:36 +0800

    Add files via upload

--- study34-39 dowilie循环/study34-39 dowilie循环/源.c
+++ study34-39 dowilie循环/study34-39 dowilie循环/源.c
@@ -174,30 +174,108 @@ int main()
 //}
 
 
+
+//if (srecmp(passward, "abcdef") == 0)  判断两个字符串是否相等不能用==，应该用库函数strcmp，返回值如果为0则二者相等 头文件<string.h>
+//int main()
+//{
+//	char passward[20] = {0};
+//	char ture[] = {"abcdef"};
+//	int sz = 0;
+//	int i = 0;
+//	int j = 0;
+//	int k = 0;
+//
+//	sz = sizeof(ture) / sizeof(ture[0]);
+//
+//
+//	printf("请输入密码\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		
+//		scanf("%s", passward);
+//		j = 0;
+//		while ( j <= (sz - 2))
+//		{
+//			if (ture[j] != passward[j])
+//			{
+//				printf("密码错误,");
+//				if (2 != i)
+//				printf("请重新输入密码\n");
+//				break; 
+//			}
+//			j++;
+//		}
+//		if (sz - 1 == j)
+//		{
+//			printf("密码正确\n");
+//			break;
+//		}
+//	}
+//	if (3 == i)
+//	{
+//		printf("次数已用尽，请明天再来\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+//猜数字游戏，电脑产生一个随机数，反馈猜大了还是猜小了
+
+
+#include <stdlib.h>
+#include <time.h>
+
+void menu()
+{
+	printf("                 \n");
+	printf("      1.play     \n");
+	printf("      0.exit     \n");
+	printf("                 \n");
+}
+
+void game()
+{
+	//1.生成随机数
+	//rand是生成随机数的函数，头文件：<stdlib.h>
+	//rand输出的顺序是一定的，可以用srand控制rand输出的起点，srand括号里的值要是一个随机数，就可以保证
+	//rand输出的是随机数 srand头文件也是<stdlib.h>
+	//time()返回值是时间戳，类型是time_t 也就是long long型的 头文件<time.h>
+	//srand 期望的参数是unsigned int 类型的所以需要强制转换一下
+	//srand((unsigned int)time(NULL));需要把这行代码移到主函数里，也就是确定一次rand的起点
+	//srand确定rand的起点，srand改变很少，对应的rand的值也改变很少
+	int ret = rand() % 100 + 1;//rand随机的数字是0~32767，%100之后变成0~99，+1之后变成1~100
+		printf("%d\n", ret);
+
+}
+
 int main()
 {
-	int sz = 0;
-	int left = 0;
-	int right = 0;
-
-	char arr1[] = { "welcome to bit!!!!" };
-	char arr2[] = { "                  " };
-	sz = sizeof(arr1) / sizeof(arr1[0]);
-	left = 0;
-	//right = sz - 2;
-	right = strlen(arr1) - 1;
-	while (left < right)
+	srand((unsigned int)time(NULL));
+	int input;
+	do
 	{
-		arr2[left] = arr1[left];
-		arr2[right] = arr1[right];
-		printf("%s\n", arr2);
-		left++;
-		right--;
-		Sleep(1000);//延时函数
-		system("cls");//清空屏幕
-	}
+		menu();
+		printf("请选择\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+			case 1:
+				game();
+				break;
+			case 0:
+				printf("退出游戏");
+				break;
+			default:
+				printf("选择错误，请重新选择\n");
+				break;
+		}
 
 
-	
+	} while (input);
+
 	return 0;
 }
+
+
